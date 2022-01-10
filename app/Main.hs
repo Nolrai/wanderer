85b6cf0fd434d88797e7ec851a060331c595130a
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2022-01-10 04:39:57 -0800

    Show's a circle!

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Main (main) where
 
+import Relude
 import Wanderer (projectName)
-
+import Graphics.Gloss
 
 main :: IO ()
-main = putStrLn ("Executable for " ++ projectName)
+main = display (InWindow ("Executable for " ++ projectName) (200, 200) (10, 10)) white (Circle 80)
+
+
