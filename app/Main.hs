8d0f1b9db46535ec917364b984c9954bc17795c5
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2022-01-10 20:22:12 -0800

    Simulation of 1 body kepler problem is complete.

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,42 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoImplicitPrelude, RecordWildCards #-}
 
 module Main (main) where
 
 import Relude
-import Wanderer (projectName)
+import Wanderer
 import Graphics.Gloss
+import Linear
 
 main :: IO ()
-main = display (InWindow ("Executable for " ++ projectName) (200, 200) (10, 10)) white (Circle 80)
+main = simulate displayMode black 32 start toPicture takeStep
+
+displayMode :: Display
+displayMode = InWindow ("Executable for " ++ projectName) (1000, 1000) (100, 100)
+
+data SimState a = SimState {parity :: Bool, p :: V2 a, q :: V2 a}
+
+start :: Num a => SimState a
+start = SimState {parity = False, p = V2 300 0, q = V2 0 10}
+
+onV2 :: (a -> a -> b) -> V2 a -> b
+onV2 f (V2 x y) = f x y
+
+toPicture :: SimState Float -> Picture
+toPicture SimState {..} =
+  pictures 
+    [ color white $ circleSolid 10
+    , onV2 translate p . color green $ circleSolid 5
+    ]
+
+-- Rotates clockwise
+takeStepTest :: a -> Float -> SimState Float -> SimState Float
+takeStepTest _ deltaT SimState {..} = SimState {parity = not parity, p = p', q = q'}
+  where
+    p' = p *! V2 (V2 (cos deltaT) (- (sin deltaT))) (V2 (sin deltaT) (cos deltaT))
+    q' = q
+
+takeStep :: a -> Float -> SimState Float -> SimState Float
+takeStep _ deltaT SimState {..} = SimState {parity = not parity, p = p', q = q'}
+  where
+    (p', q') = euler (keplerG 1000000) hookF p q deltaT
+    euler = if parity then semiImplicitEuler else semiImplicitEuler'
--- src/Wanderer.hs
+++ src/Wanderer.hs
@@ -14,6 +14,8 @@ module Wanderer
     , semiImplicitEuler'
     , keplerF
     , keplerG
+    , hookF
+    , hookG
     ) where
 
 import Relude
@@ -22,26 +24,33 @@ import Linear
 projectName :: String
 projectName = "wanderer"
 
+-- https://en.wikipedia.org/wiki/Semi-implicit_Euler_method
+-- The F/G notation is from the wikipedia page.. might be bad for physics sims.
+
 semiImplicitEuler, semiImplicitEuler' :: 
-  (t -> V2 Double -> V2 Double)
-  -> (t -> V2 Double -> V2 Double)
-  -> V2 Double
-  -> V2 Double
-  -> t
-  -> Double
-  -> (V2 Double, V2 Double)
-semiImplicitEuler g f x v t delta_t = (x', v')
+  (V2 Float -> V2 Float)
+  -> (V2 Float -> V2 Float)
+  -> V2 Float
+  -> V2 Float
+  -> Float
+  -> (V2 Float, V2 Float)
+semiImplicitEuler g f x v delta_t = (x', v')
   where
-    v', x' :: V2 Double
-    v' = v + g t x ^* delta_t
-    x' = x + f t v' ^* delta_t
+    v', x' :: V2 Float
+    v' = v + g x ^* delta_t
+    x' = x + f v' ^* delta_t
 
-semiImplicitEuler' g f x v t delta_t = (x', v')
+semiImplicitEuler' g f x v delta_t = (x', v')
   where
-    x', v' :: V2 Double
-    x' = x + f t v
-    v' = v + g t x' ^* delta_t
+    x', v' :: V2 Float
+    x' = x + f v
+    v' = v + g x' ^* delta_t
 
-keplerF, keplerG :: Double -> V2 Double -> V2 Double 
-keplerF _ v = v
-keplerG _ x = signorm x ^/ quadrance x
+-- I'm not sure if any phisics sim has something other than this..
+keplerF, hookF :: V2 Float -> V2 Float 
+keplerF v = v
+hookF = keplerF
+
+keplerG, hookG :: Float -> V2 Float -> V2 Float 
+keplerG scale x = - scale *^ signorm x ^/ quadrance x
+hookG omega x = (- omega * omega) *^ x
