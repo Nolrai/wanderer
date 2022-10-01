3af1faec37cb811a099e286aad5af344d94532d1
Chris Upshaw <chrisaupshaw@gmail.com>
2022-09-30 22:19:47 -0700

    fixed bug in reduce

--- src/GeoAlg.hs
+++ src/GeoAlg.hs
@@ -7,10 +7,14 @@ import Relude
 import Linear ( (!*!), scaled, M44, V4(V4) )
 import Data.Complex as Complex ( conjugate, realPart, Complex(..) )
 import Data.Vector as Vector ( fromList, sum, zipWith, Vector )
-import Data.Map.Strict as Map ( fromList, toAscList, mapKeysMonotonic, foldrWithKey, unionWith, intersectionWith, lookup, filter )
-import Data.Set as Set ( foldl', empty )
+import Data.Map.Strict as Map ( fromList, toAscList, mapKeysMonotonic, foldrWithKey, unionWith, lookup, filter, singleton, insertWithKey, mapWithKey, mapKeysWith, fromListWith, keysSet, keys )
+import Data.Set as Set ( foldl', empty, singleton, size, fromList, isSubsetOf, insert )
+import Text.Show (Show(show))
 
 data Axis = T | X | Y | Z
+  deriving stock (Show, Read, Ord, Eq, Enum, Bounded)
+
+data Axis' = X' | Y'
   deriving stock (Show, Read, Ord, Eq, Enum, Bounded)
 
 newtype STA a = MkSTA {asMatrix :: M44 (Complex a)}
@@ -63,16 +67,54 @@ newtype FreeModule basis a = MkFreeModule [([basis], a)]
 newtype MultiVector basis a = MkMultiVector (Map (Set basis) a)
   deriving newtype (Eq, Show, Read)
 
-reduce :: forall basis a. (Ord basis, Num a) => (basis -> a) -> [basis] -> (a, Set basis)
-reduce f l = (sig, Relude.fromList sorted)
+data Sig = Neg | Zero | Pos
+  deriving stock (Eq, Enum)
+
+instance Show Sig where
+  show :: Sig -> String
+  show x = Text.Show.show (withSig x 1 :: Int)
+
+instance Num Sig where
+  (*) = withSig
+  fromInteger 0 = Zero
+  fromInteger n
+    | n > 0 = Pos
+    | otherwise = Neg
+  negate Pos = Neg
+  negate Zero = Zero
+  negate Neg = Pos
+  Zero + x = x
+  x + Zero = x
+  x + y 
+    | x == y = x
+    | otherwise = Zero
+  abs = const Pos
+  signum = id
+
+withSig :: Num a => Sig -> a -> a
+withSig Zero = const 0
+withSig Pos = id
+withSig Neg = negate
+
+reduce :: forall basis. (Ord basis) => (basis -> Sig) -> [basis] -> (Sig, Set basis)
+reduce f l = (sig `withSig` sig', contracted)
   where
-    (sig, sorted) = go l
-    go :: [basis] -> (a, [basis])
-    go (x:y:ys)
-      | x < y = let (a', l') = go (y : ys) in (a', x : l')
-      | x == y = let (a', l') = go ys in (f x * a', l')
-      | otherwise =  let (a', l') = go (y:x:ys) in (-1 * a', l')
-    go l' = (1, l')
+    (sig', contracted) = contract f sorted
+    (sig, sorted) = gnome Pos [] l
+
+gnome :: Ord a => Sig -> [a] -> [a] -> (Sig, [a])
+gnome sig l [] = (sig, l)
+gnome sig [] (y : ys) = gnome sig [y] ys
+gnome sig (x:xs) (y:ys)
+  | x < y = gnome (negate sig) (y:xs) (x:ys)
+  | otherwise = gnome sig xs (y : x : ys)
+
+contract :: (Ord a) => (a -> Sig) -> [a] -> (Sig, Set a)
+contract _ [] = (1, Set.empty)
+contract _ [x] = (1, Set.singleton x)
+contract f (x : y : ys)
+  | x == y = let (sig, set) = contract f ys in (f x * sig, set)
+  | otherwise = let (sig, set) = contract f (y : ys) in (sig, Set.insert x set)
 
 fmProd :: Num a => FreeModule basis a -> FreeModule basis a -> FreeModule basis a
 fmProd (MkFreeModule left) (MkFreeModule right) =
@@ -81,8 +123,11 @@ fmProd (MkFreeModule left) (MkFreeModule right) =
     (rightKey, rightValue) <- right
     return (leftKey <> rightKey, leftValue * rightValue)
 
-toMV :: (Ord basis, Num a) => (basis -> a) -> FreeModule basis a -> MultiVector basis a
-toMV f (MkFreeModule l) = MkMultiVector . Map.fromList . fmap (\ (k, a) -> let (sig, k') = reduce f k in (k', sig * a)) $ l
+toMV :: (Ord basis, Num a) => (basis -> Sig) -> FreeModule basis a -> MultiVector basis a
+toMV f (MkFreeModule l) = MkMultiVector .
+  Map.fromListWith (+) .
+  map (\ (k, a) -> let (sig, k') = reduce f k in (k', sig `withSig` a)) $
+  l
 
 fromMV :: MultiVector basis a -> FreeModule basis a
 fromMV (MkMultiVector m) = MkFreeModule . Map.toAscList . Map.mapKeysMonotonic Relude.toList $ m
@@ -93,8 +138,12 @@ class Ord basis => QuadForm basis where
 
 instance QuadForm Axis where
   quadForm :: Num a => Axis -> a
-  quadForm T = 1
-  quadForm _ = -1
+  quadForm T = -1
+  quadForm _ = 1
+
+instance QuadForm Axis' where
+  quadForm :: Num a => Axis' -> a
+  quadForm _ = 1
 
 mvProd :: (QuadForm basis, Num a) => MultiVector basis a -> MultiVector basis a -> MultiVector basis a
 mvProd left right =
@@ -119,12 +168,15 @@ cdot a b = realPart $ asVector a `cdot'` asVector b
 cdot' :: RealFloat a => Vector (Complex a) -> Vector (Complex a) -> Complex a
 cdot' x y = Vector.sum $ Vector.zipWith (\ a b -> a * Complex.conjugate b) x y
 
+removeZeros :: (Eq a, Num a) => MultiVector basis a -> MultiVector basis a
+removeZeros (MkMultiVector m) = MkMultiVector . Map.filter (/= 0) $ m
+
 instance (Eq a, Fractional a, QuadForm basis) =>
   Num (MultiVector basis a) where
     (+) :: MultiVector basis a -> MultiVector basis a -> MultiVector basis a
-    MkMultiVector a + MkMultiVector b = MkMultiVector . Map.filter (/= 0) $ Map.unionWith (+) a b
+    MkMultiVector a + MkMultiVector b = removeZeros . MkMultiVector $ Map.unionWith (+) a b
     (*) :: MultiVector basis a -> MultiVector basis a -> MultiVector basis a
-    (*) = mvProd
+    a * b = removeZeros $ mvProd a b
     negate :: MultiVector basis a -> MultiVector basis a
     negate (MkMultiVector m) = MkMultiVector (negate <$> m)
     fromInteger :: Integer -> MultiVector basis a
@@ -137,3 +189,14 @@ rank0 :: (Num a, Ord basis) => MultiVector basis a -> a
 rank0 (MkMultiVector m) =
   fromMaybe 0 $ Map.lookup Set.empty m
 
+baseV :: (Ord basis, Num a) => basis -> MultiVector basis a
+baseV k = MkMultiVector $ Map.fromList [(Set.singleton k, 1)]
+
+base :: (QuadForm basis, Num a) => [basis] -> MultiVector basis a
+base l = toMV quadForm $ MkFreeModule [(l, 1)]
+
+getRanks :: MultiVector a1 a2 -> Set Int
+getRanks (MkMultiVector m) = Set.fromList . fmap Set.size . Map.keys $ m
+
+isScalar :: MultiVector a1 a2 -> Bool
+isScalar m = getRanks m `Set.isSubsetOf` Set.singleton 0
--- test/GeoAlgSpec.hs
+++ test/GeoAlgSpec.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE NoImplicitPrelude, ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Evaluate" #-}
+{-# LANGUAGE InstanceSigs #-}
 
-module GeoAlgSpec (spec) where
+module GeoAlgSpec where
 
 import GeoAlg
 import Relude
@@ -11,6 +12,7 @@ import Test.Hspec.QuickCheck
 import Test.QuickCheck
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
+import Debug.SimpleReflect ( var, Expr )
 
 spec :: Spec
 spec = do
@@ -26,6 +28,17 @@ spec = do
         \ (x :: MultiVector Axis Rational) y -> x + y `shouldBe` y + x
       prop "has absorbsive 0" $
         \ (x :: MultiVector Axis Rational) -> x * 0 `shouldBe` 0
+      prop "multiplication distributes over addition" $
+        \ (x :: MultiVector Axis Rational) (y :: MultiVector Axis Rational) (z :: MultiVector Axis Rational) -> 
+          x * (y + z) `shouldBe` (x * y) + (x * z)
+      prop "selfMultiplication produces a scalar" $
+        \ (x :: MultiVector Axis Rational) ->
+          isScalar (x * x)
+      it "multiplies" $ do
+        printLines `mapM_` toLines (multiVectorExample "a" * multiVectorExample "b")
+        True `shouldBe` False
+    prop "folows QuadForm" $ do
+      \ (t :: Axis) -> (baseV t * baseV t :: MultiVector Axis Rational) `shouldBe` fromInteger (quadForm t)
     describe "contains the Integers" $ do
       prop "fromInteger is additive homomorphism" $
         \ (x :: Integer) (y :: Integer) -> (fromInteger x :: MultiVector Axis Rational) + fromInteger y `shouldBe` fromInteger (x + y :: Integer)
@@ -47,3 +60,25 @@ instance (Ord basis, Arbitrary basis, Arbitrary t) => Arbitrary (MultiVector bas
     dims <- listOf orderedList
     values <- vector (length dims)
     pure . MkMultiVector . Map.fromList $ zipWith (\ k a -> (Set.fromList k, a)) dims values
+  shrink :: MultiVector basis t -> [MultiVector basis t]
+  shrink (MkMultiVector m) = fmap MkMultiVector (shrink m)
+
+multiVectorExample :: [Char] -> MultiVector Axis' Expr
+multiVectorExample c = MkMultiVector . Map.fromList $ do
+  (basis :: [Axis']) <- subsequences [minBound .. maxBound]
+  return (Set.fromAscList basis, toVarName c basis)
+
+toVarName :: (Eq b, Bounded b, Enum b) => String -> [b] -> Expr
+toVarName c basis = var (c <> toSubscript basis)
+  where
+  toSubscript b = (\ t -> if t `elem` b then '1' else '0') `map` [minBound .. maxBound]
+
+printLines :: (Set Axis', Expr) -> IO ()
+printLines (k, rhs) = 
+  putTextLn $ show lhs <> fromString " = " <> show rhs
+  where
+    lhs :: Expr
+    lhs = toVarName "c". Set.toAscList $ k
+
+toLines :: MultiVector basis a -> [(Set basis, a)]
+toLines (MkMultiVector m) = Map.toAscList m
